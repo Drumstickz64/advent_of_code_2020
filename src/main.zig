8770ce2aa82a6cb78dec0fa3ce7eec8b5810b6ee
Drumstickz64 <gearman533@gmail.com>
2024-10-16 00:55:09 +0300

    day 1

--- src/main.zig
+++ src/main.zig
@@ -11,8 +11,8 @@ pub fn main() !void {
     const input = if (is_test) try getInput("test_input.txt") else try getInput("input.txt");
 
     switch (part) {
-        1 => part1(input),
-        2 => part2(input),
+        1 => try part1(input),
+        2 => try part2(input),
         else => return error.IncorrectPart,
     }
 }
@@ -23,10 +23,54 @@ fn getInput(path: []const u8) ![]u8 {
     return std.fs.cwd().readFileAlloc(ally, path, std.math.maxInt(usize));
 }
 
-fn part1(input: []const u8) void {
-    std.debug.print("part 1, input: {s}\n", .{input});
+fn part1(input: []const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const ally = gpa.allocator();
+
+    var lines = std.mem.splitScalar(u8, input, '\n');
+
+    var numbers = std.ArrayList(i32).init(ally);
+    defer numbers.deinit();
+
+    while (lines.next()) |line| {
+        const number: i32 = try std.fmt.parseInt(i32, line, 10);
+        try numbers.append(number);
+    }
+
+    for (0..numbers.items.len - 1) |i| {
+        for (i..numbers.items.len) |j| {
+            if (numbers.items[i] + numbers.items[j] == 2020) {
+                const stdout = std.io.getStdOut().writer();
+                try stdout.print("answer: {d}\n", .{numbers.items[i] * numbers.items[j]});
+                return;
+            }
+        }
+    }
 }
 
-fn part2(input: []const u8) void {
-    std.debug.print("part 2, input: {s}\n", .{input});
+fn part2(input: []const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const ally = gpa.allocator();
+
+    var lines = std.mem.splitScalar(u8, input, '\n');
+
+    var numbers = std.ArrayList(i32).init(ally);
+    defer numbers.deinit();
+
+    while (lines.next()) |line| {
+        const number: i32 = try std.fmt.parseInt(i32, line, 10);
+        try numbers.append(number);
+    }
+
+    for (0..numbers.items.len - 2) |i| {
+        for (i..numbers.items.len - 1) |j| {
+            for (j..numbers.items.len) |k| {
+                if (numbers.items[i] + numbers.items[j] + numbers.items[k] == 2020) {
+                    const stdout = std.io.getStdOut().writer();
+                    try stdout.print("answer: {d}\n", .{numbers.items[i] * numbers.items[j] * numbers.items[k]});
+                    return;
+                }
+            }
+        }
+    }
 }
