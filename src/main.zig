f2f9738a635e3f23f2353f2a3b481abfa95eccfa
Drumstickz64 <gearman533@gmail.com>
2024-10-16 16:46:45 +0300

    refactor

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Parser = @import("parser.zig").Parser;
 
 pub fn main() !void {
     var args = std.process.args();
@@ -75,50 +76,4 @@ const Entry = struct {
 const Policy = struct {
     char: u8,
     positions: [2]usize,
-};
-
-const Parser = struct {
-    str: []const u8,
-    curr: usize = 0,
-
-    pub fn nextInt(self: *Parser, comptime T: type) !T {
-        const start = self.curr;
-
-        while (self.peek()) |char| {
-            if (!std.ascii.isDigit(char)) {
-                break;
-            }
-
-            _ = self.next();
-        }
-
-        return std.fmt.parseInt(T, self.str[start..self.curr], 10);
-    }
-
-    pub fn remainder(self: *Parser) []const u8 {
-        return self.str[self.curr..];
-    }
-
-    pub fn expect(self: *Parser, expected: u8) !void {
-        if (self.next() != expected) {
-            return error.ExpectMismatch;
-        }
-    }
-
-    pub fn next(self: *Parser) ?u8 {
-        if (self.curr >= self.str.len) {
-            return null;
-        }
-
-        self.curr += 1;
-        return self.str[self.curr - 1];
-    }
-
-    pub fn peek(self: *Parser) ?u8 {
-        if (self.curr >= self.str.len) {
-            return null;
-        }
-
-        return self.str[self.curr];
-    }
 };
