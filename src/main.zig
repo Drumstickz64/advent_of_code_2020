6872766ee8abbac3d75cb442ae1bd51a0901adfe
Drumstickz64 <gearman533@gmail.com>
2024-10-16 16:19:05 +0300

    day 2 part 1

--- src/main.zig
+++ src/main.zig
@@ -3,18 +3,14 @@ const std = @import("std");
 pub fn main() !void {
     var args = std.process.args();
     _ = args.skip();
-    const part_str = args.next() orelse return error.PartNotProvided;
-    const part = try std.fmt.parseInt(u8, part_str, 10);
 
     const is_test = if (args.next()) |is_test_str| std.mem.eql(u8, is_test_str, "test") else false;
 
     const input = if (is_test) try getInput("test_input.txt") else try getInput("input.txt");
 
-    switch (part) {
-        1 => try part1(input),
-        2 => try part2(input),
-        else => return error.IncorrectPart,
-    }
+    const answer = try solve(input);
+    const writer = std.io.getStdOut().writer();
+    try writer.print("answer: {any}\n", .{answer});
 }
 
 fn getInput(path: []const u8) ![]u8 {
@@ -23,54 +19,106 @@ fn getInput(path: []const u8) ![]u8 {
     return std.fs.cwd().readFileAlloc(ally, path, std.math.maxInt(usize));
 }
 
-fn part1(input: []const u8) !void {
+fn solve(input: []const u8) !u64 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const ally = gpa.allocator();
 
     var lines = std.mem.splitScalar(u8, input, '\n');
 
-    var numbers = std.ArrayList(i32).init(ally);
-    defer numbers.deinit();
+    var entries = std.ArrayList(Entry).init(ally);
+    defer entries.deinit();
 
     while (lines.next()) |line| {
-        const number: i32 = try std.fmt.parseInt(i32, line, 10);
-        try numbers.append(number);
+        const entry = try Entry.parse(line);
+        try entries.append(entry);
     }
 
-    for (0..numbers.items.len - 1) |i| {
-        for (i..numbers.items.len) |j| {
-            if (numbers.items[i] + numbers.items[j] == 2020) {
-                const stdout = std.io.getStdOut().writer();
-                try stdout.print("answer: {d}\n", .{numbers.items[i] * numbers.items[j]});
-                return;
-            }
+    var validEntryCount: u64 = 0;
+    for (entries.items) |entry| {
+        const count = std.mem.count(u8, entry.password, &[_]u8{entry.policy.char});
+        if (entry.policy.min <= count and count <= entry.policy.max) {
+            validEntryCount += 1;
         }
     }
+
+    return validEntryCount;
 }
 
-fn part2(input: []const u8) !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const ally = gpa.allocator();
+const Entry = struct {
+    policy: Policy,
+    password: []const u8,
 
-    var lines = std.mem.splitScalar(u8, input, '\n');
+    pub fn parse(entry_str: []const u8) !Entry {
+        var parser = Parser{ .str = entry_str };
 
-    var numbers = std.ArrayList(i32).init(ally);
-    defer numbers.deinit();
+        const min = try parser.nextInt(u32);
+        try parser.expect('-');
+        const max = try parser.nextInt(u32);
+        try parser.expect(' ');
+        const char = parser.next().?;
+        try parser.expect(':');
+        try parser.expect(' ');
+        const password = parser.remainder();
 
-    while (lines.next()) |line| {
-        const number: i32 = try std.fmt.parseInt(i32, line, 10);
-        try numbers.append(number);
+        return Entry{
+            .policy = Policy{
+                .min = min,
+                .max = max,
+                .char = char,
+            },
+            .password = password,
+        };
+    }
+};
+
+const Policy = struct {
+    char: u8,
+    min: u32,
+    max: u32,
+};
+
+const Parser = struct {
+    str: []const u8,
+    curr: usize = 0,
+
+    pub fn nextInt(self: *Parser, comptime T: type) !T {
+        const start = self.curr;
+
+        while (self.peek()) |char| {
+            if (!std.ascii.isDigit(char)) {
+                break;
+            }
+
+            _ = self.next();
+        }
+
+        return std.fmt.parseInt(T, self.str[start..self.curr], 10);
     }
 
-    for (0..numbers.items.len - 2) |i| {
-        for (i..numbers.items.len - 1) |j| {
-            for (j..numbers.items.len) |k| {
-                if (numbers.items[i] + numbers.items[j] + numbers.items[k] == 2020) {
-                    const stdout = std.io.getStdOut().writer();
-                    try stdout.print("answer: {d}\n", .{numbers.items[i] * numbers.items[j] * numbers.items[k]});
-                    return;
-                }
-            }
+    pub fn remainder(self: *Parser) []const u8 {
+        return self.str[self.curr..];
+    }
+
+    pub fn expect(self: *Parser, expected: u8) !void {
+        if (self.next() != expected) {
+            return error.ExpectMismatch;
         }
     }
-}
+
+    pub fn next(self: *Parser) ?u8 {
+        if (self.curr >= self.str.len) {
+            return null;
+        }
+
+        self.curr += 1;
+        return self.str[self.curr - 1];
+    }
+
+    pub fn peek(self: *Parser) ?u8 {
+        if (self.curr >= self.str.len) {
+            return null;
+        }
+
+        return self.str[self.curr];
+    }
+};
